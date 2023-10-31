f01462ab54ad67a97bb9d838e666810dab01328b
Abhishek Rajopadhye <21f1004638@student.onlinedegree.iitm.ac.in>
2023-10-31 23:05:11 +0530

    Video responsive

--- src/views/NBackTestView.vue
+++ src/views/NBackTestView.vue
@@ -20,6 +20,16 @@
 			This is called the N=2 back test, because you are comparing the current letter to the letter that was displayed
 			two letters ago.<br />
 		</p>
+		<div class="demo-video">
+			<center>
+				<h3>
+					Here is a demo video of the test below<br />
+				</h3>
+				<video controls preload="metadata">
+					<source src="/src/assets/nbackdemo.mp4" type="video/mp4">
+				</video>
+			</center>
+		</div>
 		<br />
 	</div>
 	<div class="real-test-indicator" v-if="this.trialCompleted && !this.testStartFlag">
@@ -114,7 +124,9 @@ export default {
 }
 
 video {
-	border: 1px solid
+	border: 1px solid;
+	width: 100%;
+	height: auto
 }
 
 .start-button {
