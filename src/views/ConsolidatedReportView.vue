d6ad7165a4fa0c1c502b15d4e1f66ee44469c9b0
Abhishek Rajopadhye <darthrecca@gmail.com>
2024-01-10 06:11:29 +0530

    UpdatedReport+ReportPDFDownload

--- src/views/ConsolidatedReportView.vue
+++ src/views/ConsolidatedReportView.vue
@@ -3,55 +3,117 @@
 		<h3>Consolidated Report</h3>
 		<br />
 	</div>
-	<div class="report">
-		<br />
+	<div >
+		<v-btn @click="exportToPDF()" color="black" rounded="0" prepend-icon="mdi-download">Download PDF Report</v-btn>
+	</div>
+	<div class="report" ref="report" id="report">
 		<center>
-			<table>
+			<p><span>Result of the memory test based on computer analysis</span></p>
+			<br />
+			<table class="user-details">
+				<tr>
+					<td class="type-field">Name</td>
+					<td class="value-field">{{ this.data.userDetailsData.name }}</td>
+				</tr>
+				<tr>
+					<td class="type-field">Email ID</td>
+					<td class="value-field">{{ this.data.userDetailsData.emailID }}</td>
+				</tr>
+				<tr>
+					<td class="type-field">Gender</td>
+					<td class="value-field">{{ this.data.userDetailsData.gender }}</td>
+				</tr>
+			</table>
+			<br/>
+			<table class="info">
+				<tr>
+					<td class="type-field">Please note this is a result of a computerized memory test. This is NOT a medical diagnosis</td>
+				</tr>
+				<tr>
+					<td class="type-field">कृपया लक्षात घ्या की हा संगणकीकृत मेमरी चाचणीचा निकाल आहे. हा निकाल म्हणजे वैद्यकीय निदान नाही</td>
+				</tr>
+				<tr>
+					<td class="type-field">कृपया ध्यान दें कि यह आपके संगणक द्वारे किया हुआ मेमरी टेस्ट का परिणाम है। यह परिणाम वैद्यकीय निदान नहीं है।</td>
+				</tr>
+			</table>
+			<br/>
+			<table class="results-table">
 				<thead>
 					<tr>
-						<th>Test</th>
-						<th>Score</th>
+						<th class="head-field">Assessment</th>
+						<th class="head-field">Observations Based on Computer Analysis of your responses</th>
+						<th class="head-field">Expected Results</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
-						<td>Short Term Memory Words Recalled Correctly(out of 10)</td>
-						<td>{{ this.data.shortTermMemoryTestData.score }}</td>
+						<td class="type-field">Short Term Memory Test</td>
+						<td class="value-field">You recalled {{ this.data.shortTermMemoryTestData.score }} words correctly out of 10</td>
+						<td class="value-field" rowspan="2">
+							Age group 12 to 20: around 7 to 8<br/>
+  							Age group 20 to 60: around 6-7<br/>
+  							Age group 60+: around 5 to 6
+						</td>
 					</tr>
 					<tr>
-						<td>Maximum length of digits that you can remember (Digit Span)</td>
-						<td>{{ this.data.sequenceDigitSpanTestData.highestSequenceDigitSpan }}</td>
+						<td class="type-field">Long Term Memory Test</td>
+						<td class="value-field">You recalled {{ this.data.longTermMemoryTestData.score }} words correctly out of 10</td>
+					</tr>
+					<tr class="stroop-results">
+						<td class="type-field">Stroop Test(for Inhibition Control)</td>
+						<td class="value-field">Your Stroop Score (Incongruent Avg Time - Congruent Avg Time) is {{ this.data.stroopTestData.testScore }} ms</td>
+						<td class="value-field">
+							Age group 12 to 20: 50-100 ms<br/>
+  							Age group 20 to 60: 75-150 ms<br/>
+  							Age group 60+: 100-200 ms
+						</td>
+					</tr>
+					<tr class="nback-results">
+						<td class="type-field">N-2 Back test(for Working Memory)</td>
+						<td class="value-field">Your Percentage of correct tasks in trials that had match was {{ this.data.nBackTestData.nBackTestScore }}%</td>
+						<td class="value-field">
+							Age group 12 to 20: 60-80 % <br/>
+  							Age group 20 to 60: 80-90 %<br/>
+  							Age group 60+: 70-80 %
+						</td>
 					</tr>
 					<tr>
-						<td>Maximum length of digits that you can remember in Reverse order (Reverse Digit Span)</td>
-						<td>{{ this.data.sequenceDigitSpanTestData.highestSequenceDigitSpan }}</td>
+						<td class="type-field">Shape Digit Coding(for measuring Processing Speed)</td>
+						<td class="value-field">Number of shapes correctly coded in 2 mins by you was {{ this.data.shapeDigitCodeTestData.shapeDigitCodeTestScore }}</td>
+						<td class="value-field">40+</td>
 					</tr>
 					<tr>
-						<td>Maximum length of digits that you can remember and organise in Order(Ascending Order)</td>
-						<td>{{ this.data.sequenceDigitSpanTestData.highestSequenceDigitSpan }}</td>
+						<td class="type-field">Digit Span Test(for Short Term Memory)</td>
+						<td class="value-field">Maximum length of digits that you can remember {{ this.data.digitSpanTestData.highestDigitSpan }}</td>
+						<td class="value-field">5 to 9</td>
 					</tr>
 					<tr>
-						<td>Your Stroop Score (Incongruent Avg Time - Congruent Avg Time)</td>
-						<td>{{ this.data.stroopTestData.testScore }} ms</td>
+						<td class="type-field">Reverse Digit Span Test(for Short Term Memory and Working Memory)</td>
+						<td class="value-field">Maximum length of digits that you can remember in REVERSE order (REVERSE Digit Span) is {{ this.data.reverseDigitSpanTestData.highestReverseDigitSpan }}</td>
+						<td class="value-field" rowspan="2">4 to 8</td>
 					</tr>
-					<tr>
-						<td>NBack Test Trials Correct Percentage(for trials that had a match)</td>
-						<td>{{ this.data.nBackTestData.performanceParameters.matchTrialsCorrectPercent }} %</td>
-					</tr>
-					<tr>
-						<td>Number of Shapes Digit Coded Correctly</td>
-						<td>{{ this.data.shapeDigitCodeTestData.shapeDigitCodeTestScore }}</td>
-					</tr>
-					<tr>
-						<td>Long Term Memory Test Words Recalled Correctly(out of 10)</td>
-						<td>{{ this.data.longTermMemoryTestData.score }}</td>
+					<tr class="sequence-digit-span-results">
+						<td class="type-field">Ordered Digit Span Test(for Short Term Memory and Working Memory)</td>
+						<td class="value-field">Maximum length of digits that you can remember and organize in ascending order is {{ this.data.sequenceDigitSpanTestData.highestSequenceDigitSpan }}</td>
 					</tr>
 				</tbody>
+				<tfoot class="footer">
+					<tr class="footer-1">
+						<td class="foot-field" colspan="3">It is well-known that you can improve your memory and other brain functions by training the brain.<br/>
+							The best part of this memory test is, it also serves as a <b>Brain Trainer</b>.<br/>
+							Practice the test every alternate day to improve your score and brain function.</td>
+					</tr>
+					<tr>
+						<td class="value-field"><b>Do you have less score than expected results?</b><br/>Do not be panic. The number can vary depending on the age, attention, overall mind health and memory strategies. You can take the test many times. Consult the expert if your results are consistently low.</td>
+						<td class="value-field"><b>तुमचा निकाल अपेक्षित निकालापेक्षा कमी आहे का?</b><br/>काळजी नको. तुमचा निकाल तुमचे वय, दिलेले लक्ष, एकूणच मनाचे आरोग्य आणि स्मरण करण्याचे धोरण यानुसार संख्या बदलू शकतात. परत टेस्ट द्या आणि जर निकाल नेहमीच कमी असतील तर तज्ज्ञांचा सल्ला घ्या.</td>
+						<td class="value-field"><b>क्या आपका परिणाम अपेक्षित परिणाम से कम है?</b><br/>चिंता मत करो. आपके परिणाम आपकी उम्र, दिया हुआ ध्यान, समग्र दिमागी स्वास्थ्य और स्मृति रणनीतियों के आधार पर भिन्न हो सकती है। दोबारा परीक्षण करे और यदी परिणाम लगातार कम आ रहे हैं, तो किसी विशेषज्ञ से परामर्श लें।</td>
+					</tr>
+				</tfoot>
 			</table>
 		</center>
 		<br />
 	</div>
-	<br />
+	<br/>
 	<div class="next-button">
 		<v-btn to="/endscreen" size="x-large" block color="red-lighten-3" rounded="lg">Next</v-btn>
 	</div>
@@ -59,7 +121,7 @@
 
 <script>
 import { useTestStore } from '@/store/tests';
-
+import html2pdf from 'html2pdf.js'
 export default {
 	name: 'ConsolidatedReportView',
 	data() {
@@ -152,6 +214,21 @@ export default {
 	methods: {
 		getData() {
 			this.data = useTestStore().getData();
+		},
+		exportToPDF(){
+			var opt = {
+						margin:       1,
+						filename:     'Report_'+ this.data.userDetailsData.name +'.pdf',
+						pagebreak:{
+							before:'.nback-results',
+							after:['.footer-1']
+						},
+						image:{
+							quality:1,
+							type:'png'
+						}
+			}
+			html2pdf().set(opt).from(document.getElementById('report')).save();
 		}
 	},
 	mounted() {
@@ -159,6 +236,7 @@ export default {
 	}
 };
 </script>
+
 <style>
 .report-title {
 	align-items: center;
@@ -174,7 +252,6 @@ export default {
 
 table {
 	border: 3px solid black;
-	background-color: aqua;
 	border-collapse: collapse;
 	overflow-x: auto;
 }
@@ -194,6 +271,28 @@ td {
 	border: 1px solid black;
 	padding: 10px;
 	text-align: left;
+}
+
+.results-table{
+	margin-left: 15px;
+	margin-right: 15px;
+}
+
+.head-field{
+	background-color: #339499;
+}
+
+.foot-field{
+	background-color: #339499;
+	text-align: center;
+}
+
+.type-field{
+	background-color: #05C7B0;
+}
+
+.value-field{
+	background-color: white;
 }
 
 .report {
