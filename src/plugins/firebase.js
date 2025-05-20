bb03ac165378d31254ac2a59783f19f55212b448
Abhishek Rajopadhye <darthrecca@gmail.com>
2025-05-20 11:50:21 +0530

    Update firebase.js

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -5,14 +5,14 @@ import { getAuth } from 'firebase/auth'; // Import other Firebase services as ne
 const FirebasePlugin = {
 	install(app) {
 		const firebaseConfig = {
-			apiKey: 'AIzaSyDaiNAm8P2kssBmqHQrYbZXfmAvZPjF6iQ',
-			authDomain: 'memorytests-2788d.firebaseapp.com',
-			databaseURL: 'https://memorytests-2788d-default-rtdb.firebaseio.com',
-			projectId: 'memorytests-2788d',
-			storageBucket: 'buckets/memorytests-2788d.appspot.com',
-			messagingSenderId: '843804225888',
-			appId: '1:843804225888:web:5d755d447c43a782edae05',
-			measurementId: 'G-8KK4E4E1RE'
+			apiKey: import.meta.env.VITE_firebase_api_key,
+			authDomain: import.meta.env.VITE_firebase_auth_domain,
+			databaseURL: import.meta.env.VITE_firebase_database_url,
+			projectId: import.meta.env.VITE_firebase_project_id,
+			storageBucket: import.meta.env.VITE_firebase_storage_bucket,
+			messagingSenderId: import.meta.env.VITE_firebase_messaging_sender_id,
+			appId: import.meta.env.VITE_firebase_app_id,
+			measurementId: import.meta.env.VITE_firebase_measurement_id
 		};
 
 		const firebaseApp = initializeApp(firebaseConfig);
